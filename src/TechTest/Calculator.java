201e19634675222844a27dc8122d4fbe1e586759
Voila-Voici <euroru98@gmail.com>
2023-09-06 11:29:38 +0300

    Aaa the new exception

--- src/TechTest/Calculator.java
+++ src/TechTest/Calculator.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 
 public class Calculator {
     public static int calc(String input) throws IOException {
-        String[] arr = input.split(" "); // 3 # 2
-        int ch1 = Integer.parseInt(arr[0]); // ["3", "#", "2"]
+        if (!input.contains(" ")){
+            throw new IOException("При вводе отсутствует пробел между символами");
+        }
+        String[] arr = input.split(" ");
+        int ch1 = Integer.parseInt(arr[0]);
         if ((ch1 > 10) || (ch1 < 1)) {
             throw new IOException("Неподходящее число");
         }
@@ -14,11 +17,13 @@ public class Calculator {
         if ((ch2 > 10) || (ch2 < 1)) {
             throw new IOException("Неподходящее число");
         }
-//
+        //
         if (!arr[1].equals("+") && !arr[1].equals("-") && !arr[1].equals("*") && !arr[1].equals("/")){
             throw new IOException("Неподходящее операция, действие не может быть произведено");
         }
-
+        if (arr.length > 4) {
+            throw new IOException("Превышена длина выражения");
+        }
         int result;
         switch (arr[1]) {
             case "+":
